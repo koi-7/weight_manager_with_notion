ac80179b1f5b224d8070240658c516bbe30e79e3
Koi <kkoiwa.1720+git@gmail.com>
2022-12-21 14:07:08 +0900

    add --set option

--- weight_manager_with_notion/main.py
+++ weight_manager_with_notion/main.py
@@ -2,8 +2,45 @@
 # coding: utf-8
 
 
+from argparse import ArgumentParser
+import os
+import sys
+
+
+MAIN_DIR = os.path.dirname(__file__)
+CONF_DIR = os.path.join(MAIN_DIR, '../conf/')
+DB_URL_PATH = os.path.join(CONF_DIR, 'db_url')
+TOKEN_PATH = os.path.join(CONF_DIR, 'token')
+
+
+def make_files(db_url, token):
+    if not os.path.isdir(CONF_DIR):
+        os.mkdir(CONF_DIR)
+
+    with open(DB_URL_PATH, 'w') as f:
+        f.write(db_url)
+    with open(TOKEN_PATH, 'w') as f:
+        f.write(token)
+
+def get_option():
+    argparser = ArgumentParser()
+
+    argparser.add_argument('--set', action='store_true', help='set db_url nad token')
+
+    return argparser.parse_args()
+
 def main():
-    pass
+    if len(sys.argv) == 1:
+        print('See usage with -h, --help option.')
+        sys.exit(0)
+
+    args = get_option()
+
+    if args.set:
+        db_url = input('Input DB URL: ')
+        token = input('Input Token: ')
+        make_files(db_url, token)
+        print('Setting completed.')
 
 
 if __name__ == '__main__':
