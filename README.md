a7fd0ab778369a888dab9a790e263682057042bc
Koi <kkoiwa.1720+git@gmail.com>
2022-12-28 04:06:57 +0900

    add --list, --graph, --set option

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# 体重管理アプリ
+# Weight manager with Notion
 
-Notion と連携して体重を管理するアプリ。
+Fetch weight data in Notion and show, list its data.
 
 # Usage
 
-## Options
+Set your database URL and API token.
+
+``` bash
+$ ./main --set
+Input DB URL: https://www.notion.so/<usename>/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx?v=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+Input Token: secret_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+
+Show or list your data with option.
+
+``` bash
+$ ./main <option>
+```
+
+# Options
+
+|  option  |  description  |
+| ---- | ---- |
+| -h, --help | show help message |
+| --list N | show recent N records |
+| --graph | show graph |
+| --set | set db_url nad token |
--- weight_manager_with_notion/main.py
+++ weight_manager_with_notion/main.py
@@ -3,7 +3,9 @@
 
 
 from argparse import ArgumentParser
+import matplotlib.pyplot as plt
 import os
+import requests
 import sys
 
 
@@ -12,6 +14,15 @@ CONF_DIR = os.path.join(MAIN_DIR, '../conf/')
 DB_URL_PATH = os.path.join(CONF_DIR, 'db_url')
 TOKEN_PATH = os.path.join(CONF_DIR, 'token')
 
+
+def get_option():
+    argparser = ArgumentParser()
+
+    argparser.add_argument('--graph', action='store_true', help='show graph')
+    argparser.add_argument('--list', type=int, help='show recent N records')
+    argparser.add_argument('--set', action='store_true', help='set db_url and token')
+
+    return argparser.parse_args()
 
 def make_files(db_url, token):
     if not os.path.isdir(CONF_DIR):
@@ -22,12 +33,18 @@ def make_files(db_url, token):
     with open(TOKEN_PATH, 'w') as f:
         f.write(token)
 
-def get_option():
-    argparser = ArgumentParser()
+def get_dbid():
+    with open(DB_URL_PATH, 'r') as f:
+        url =  f.readline().rstrip('\n')
+    l = url.split('/')
+    dbid = l[4].split('?')[0]
+    return dbid
 
-    argparser.add_argument('--set', action='store_true', help='set db_url nad token')
+def get_token():
+    with open(TOKEN_PATH, 'r') as f:
+        token = f.readline().rstrip('\n')
+    return token
 
-    return argparser.parse_args()
 
 def main():
     if len(sys.argv) == 1:
@@ -41,6 +58,41 @@ def main():
         token = input('Input Token: ')
         make_files(db_url, token)
         print('Setting completed.')
+        sys.exit(0)
+
+    dbid = get_dbid()
+    token = get_token()
+
+    req_url = 'https://api.notion.com/v1/databases/' + dbid + '/query'
+
+    if args.graph or (args.list != None):
+        headers = {'Authorization': 'Bearer ' + token,
+                   'Content-Type': 'application/json; charset=UTF-8',
+                   'Notion-Version': '2022-06-28'}
+        response = requests.post(url=req_url, headers=headers)
+        json_data = response.json()
+        results = json_data.get('results')
+        date_list = [r['properties']['Date']['title'][0]['text']['content'] for r in results]
+        weight_list = [r['properties']['Weight']['number'] for r in results]
+
+        dic = dict(zip(date_list, weight_list))
+        dic = dict(sorted(dic.items()))
+
+        date_list_sorted = list(dic.keys())
+        weight_list_sorted = list(dic.values())
+
+        if args.graph:
+            plt.plot(date_list_sorted, weight_list_sorted)
+            plt.xticks(rotation=90)
+            plt.show()
+
+        if args.list != None:
+            dlist = date_list_sorted[-args.list:]
+            wlist = weight_list_sorted[-args.list:]
+            for i in range(len(dlist)):
+                print('   ' + dlist[i] + ': ' + str(wlist[i]) + ' kg')
+
+        sys.exit(0)
 
 
 if __name__ == '__main__':
